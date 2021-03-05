fd31af2caa1ee9877f1f12f3a34e529f962636f2
joeyhuaa <joeyhua17@gmail.com>
2021-03-05 15:03:06 -0800

    fix bug

--- src/components/MusicPlayer.js
+++ src/components/MusicPlayer.js
@@ -1,4 +1,4 @@
-import React, {useState, useRef, useEffect} from 'react'
+import React, {useState, useRef} from 'react'
 import play from '../assets/play.png'
 import pause from '../assets/pause.png'
 import moment from 'moment'
